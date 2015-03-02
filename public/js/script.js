59302deb150e051c49f470c84833975283d55777
Jerome Martinez <jerome.e.martinez@gmail.com>
2015-03-02 01:05:47 -0600

    working on integrating p5 with socket

--- public/js/script.js
+++ public/js/script.js
@@ -5,84 +5,23 @@
     // some canvas setup ugh //
     ///////////////////////////
 
-    // i freakin hate hungarians :(
-    var $doc = $(document),
-        $win = $(window),
-        $canvas = $('#park'),   // element
-        context = $canvas[0].getContext('2d');
 
-    // dynamic full screen
-    window.addEventListener('resize', resizeCanvas, false);
-        function resizeCanvas() {
-            $canvas[0].width = window.innerWidth;
-            $canvas[0].height = window.innerHeight;
-            draw();
-    } resizeCanvas();
 
-    // hiding your cursor
-    $("body").css('cursor', 'url(../img/kite.png),auto');
+    // // dynamic full screen
+    // window.addEventListener('resize', resizeCanvas, false);
+    //     function resizeCanvas() {
+    //         $canvas[0].width = window.innerWidth;
+    //         $canvas[0].height = window.innerHeight;
+    // } resizeCanvas();
 
+    // // hiding your cursor
+    // $("body").css('cursor', 'url(../img/kite.png),auto');
+    var myp5 = new p5(sketch);
 
     /////////////////
     // socket shit //
     /////////////////
 
     // shh listening 👀👂))
-    var socket = io();
-
-    // create unique id
-    var id = Math.round($.now()*Math.random());
-    var clients = {};   // fliers
-    var cursors = {};   // their kites
-    var lastEmit = $.now();
-    
-    // capture movement
-    socket.on('moving', function (data) {
-
-        // a new user has come online
-        // create a cursor for them
-        if (!(data.id in clients)) {
-            cursors[data.id] = $('<div class="cursor">').appendTo('#cursors');
-        }
-
-        // move the mouse pointer
-        cursors[data.id].css({
-            'left': data.x,
-            'top' : data.y
-        });
-
-        // saving the current client state
-        clients[data.id] = data;
-        clients[data.id].updated = $.now();
-    });
-
-    // capture current movements
-    $doc.on('mousemove',function(e){
-        if($.now() - lastEmit > 30){
-            socket.emit('mousemove',{
-                'x': e.pageX,
-                'y': e.pageY,
-                'id': id
-            });
-            lastEmit = $.now();
-        }
-    });
-
-    // kill clients after ten seconds of inactivity
-    setInterval(function(){
-        for(ident in clients){
-            if($.now() - clients[ident].updated > 10000){
-                // last update was more than 10 seconds ago: kill user
-                cursors[ident].remove();
-                delete clients[ident];
-                delete cursors[ident];
-            }
-        }
-    },10000);
-
-    // do all canvas drawing IN HERE
-    function draw() {
-        // are we drawing anything here?
-    }
 
 })();
---
+++ public/js/sketch.js
@@ -0,0 +1,92 @@
+var sketch = function(p) {
+
+    var $doc = $(document);
+    var $win = $(window);
+
+    var id = Math.round($.now() * Math.random());
+    var clients = {};   // fliers
+    var cursors = {};   // their kites
+    var lastEmit = $.now();
+
+    var socket = io();
+    var yourKite;
+    var bg = p.loadImage("../img/zilker.jpg");
+
+    document.body.style.cursor = 'none';
+
+    // emit events when the docmument finds mouse movements
+    $doc.on('mousemove',function(e){
+        console.log('doc moging')
+        if ($.now() - lastEmit > 30){
+            socket.emit('mousemove',{
+                'x': p.mouseX,
+                'y': p.mouseY,
+                'id': id
+            });
+            lastEmit = $.now();
+        }
+    });
+
+    // kill clients after ten seconds of inactivity
+    setInterval(function(){
+        console.log('losing kite')
+        for (ident in clients){
+            if ($.now() - clients[ident].updated > 10000){
+                // last update was more than 10 seconds ago: kill user
+                cursors[ident].remove();
+                delete clients[ident];
+                delete cursors[ident];
+            }
+        }
+    }, 10000);
+
+    socket.on('moving', function (data) {
+        console.log('movinegg here', data);
+        // a new user has come online
+        // create a cursor for them
+        if (!(data.id in clients)) {
+            console.log('adding new cursor', id);
+            cursors[data.id] = new Kite(p.mouseX, p.mouseY);
+        }
+
+        // reassign this mouse data
+        cursors[data.id].x = p.mouseX;
+        cursors[data.id].y = p.mouseY
+
+        clients[data.id] = data;
+        clients[data.id].updated = $.now();
+    });
+
+    p.setup = function() {
+        p.background(bg);
+
+        yourKite = new Kite(p.mouseX, p.mouseY);
+        p.createCanvas(p.windowWidth, p.windowHeight);
+    };
+
+    p.draw = function() {
+        // console.log('clients', clients);
+
+        p.background(bg);
+        yourKite.displaySelf();
+        for (var ident = clients.length - 1; ident >= 0; ident--) {
+            cursors[ident.id].display();
+        };
+
+    };
+
+    // kite class
+    function Kite(x, y) {
+        this.x = x;
+        this.y = y;
+        this.diameter = Math.random(10, 30);
+
+        this.display = function() {
+            p.ellipse(this.x, this.y, this.diameter, this.diameter);
+        }
+
+        this.displaySelf = function() {
+            p.ellipse(p.mouseX, p.mouseY, 50, 50);
+        }
+    }
+};
